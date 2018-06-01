63273db72d54b328bf3fe5cdfe081a876d829231
pedrolima25 <39833885+pedrolima25@users.noreply.github.com>
2018-06-01 12:37:46 -0300

    Add files via upload
    br.com.alura.algoritmos;
    
    public class TestaMenorPreco {
    
    	public static void main(String[] args) {
    		
    		double precos[] = new double[5];
    		precos[0] = 1000000;
    		precos[1] = 46000;
    		precos[2] = 16000;
    		precos[3] = 46000;
    		precos[4] = 17000;
    		
    	int maisBarato = 0;
    	for(int atual = 0; atual <= 4; atual++){ 
    	     if(precos[atual] < precos[maisBarato]) {
    			maisBarato = atual;
    		}
    	}	
    	System.out.println(maisBarato);
    	System.out.println("O carro mais barato custa " + precos[maisBarato]);
    	
     }
    	
    }

---
+++ TestaMenorPreco.java
@@ -0,0 +1,25 @@
+package br.com.alura.algoritmos;
+
+public class TestaMenorPreco {
+
+	public static void main(String[] args) {
+		
+		double precos[] = new double[5];
+		precos[0] = 1000000;
+		precos[1] = 46000;
+		precos[2] = 16000;
+		precos[3] = 46000;
+		precos[4] = 17000;
+		
+	int maisBarato = 0;
+	for(int atual = 0; atual <= 4; atual++){ 
+	     if(precos[atual] < precos[maisBarato]) {
+			maisBarato = atual;
+		}
+	}	
+	System.out.println(maisBarato);
+	System.out.println("O carro mais barato custa " + precos[maisBarato]);
+	
+ }
+	
+}	
